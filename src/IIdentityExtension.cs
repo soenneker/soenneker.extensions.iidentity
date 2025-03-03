151f2b821a048f0e000822894f4603c7f5098f6a
Jake Soenneker <jake@soenneker.com>
2025-03-03 10:28:28 -0600

    Initial commit

---
+++ src/IIdentityExtension.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using Soenneker.Extensions.String;
+
+namespace Soenneker.Extensions.IIdentity;
+
+/// <summary>
+/// A collection of helpful IIdentity (authentication, authorization) extension methods
+/// </summary>
+// ReSharper disable once InconsistentNaming
+public static class IIdentityExtension
+{
+    public static void AddRolesFromJobTitle(this System.Security.Principal.IIdentity? identity)
+    {
+        if (identity is not ClaimsIdentity claimsIdentity)
+            return;
+
+        Claim? jobTitle = claimsIdentity.FindFirst("jobTitle");
+
+        if (jobTitle == null)
+            return;
+
+        List<string> roles = jobTitle.Value.FromCommaSeparatedToList();
+
+        var claims = new List<Claim>();
+
+        for (var i = 0; i < roles.Count; i++)
+        {
+            string role = roles[i];
+            claims.Add(new Claim(ClaimTypes.Role, role.Trim()));
+        }
+
+        if (claims.Count > 0)
+            claimsIdentity.AddClaims(claims);
+    }
+}
---
+++ test/Soenneker.Extensions.IIdentity.Tests/IIdentityExtensionTests.cs
@@ -0,0 +1,18 @@
+﻿using Soenneker.Tests.FixturedUnit;
+using Xunit;
+
+namespace Soenneker.Extensions.IIdentity.Tests;
+
+[Collection("Collection")]
+public class IIdentityExtensionTests : FixturedUnitTest
+{
+    public IIdentityExtensionTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    {
+    }
+
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
